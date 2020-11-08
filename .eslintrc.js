78021e8b1767e044cf23723c75127cccab07110d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-08 08:38:21 -0300

    Configure lint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "@jungsoft/eslint-config/react",
+    "@jungsoft/eslint-config/typescript",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+      node: {
+        extensions: [
+          ".ts",
+          ".tsx",
+        ],
+      },
+    },
+  },
+};
